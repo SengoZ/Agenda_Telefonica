bfb148d6f65c6edccdbe13c426f5440abe4afc2e
admin <amadorcaceresrubio@gmail.com>
2019-03-26 14:26:09 +0100

    Provincia ->incluir ToString añadir REST Listar + ADD + javadoc y documentacion

--- Server-RestAgenda/src/main/java/com/spring/agenda/control/AgendaController.java
+++ Server-RestAgenda/src/main/java/com/spring/agenda/control/AgendaController.java
@@ -12,26 +12,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.agenda.model.Direccion;
 import com.spring.agenda.model.Persona;
+import com.spring.agenda.model.Provincia;
 import com.spring.agenda.model.Telefono;
 import com.spring.agenda.services.AgendaService;
-
+/**
+ * Clase AgendaController. Realiza y gestiona las peticiones REST que lleguen para listar, añadir, borrar y editar un contacto
+ * @author Grupo 3: Amador Cáceres, Cesar Marcos, Andrés Gomez y Sheila García
+ *
+ */
 @RestController
 @RequestMapping({"/person"})
 public class AgendaController {
 
+	/**
+	 * Inyección en la interface AgendaService de su implementacion
+	 */
 	@Autowired
 	private AgendaService servicios;
-	
+	 /**
+	  * Método para listar los contactos existentes en la BBDD
+	  * @return List<Personas>: devuelve una lista de tipo Personas
+	  */
 	@GetMapping
     public List<Persona> list(){
-        System.out.println("-----------------------------------------------------------------listaaa");
         return servicios.list();
     }
+	
+	/**
+	 * Método para añadir un contacto a la base de datos
+	 * @param pers: Recoge el objeto tipo Json y introduce los atributos correspondientes de persona
+	 * @param tel: Recoge el objeto tipo Json y introduce los atributos correspondientes de Telefono
+	 * @param dir: Recoge el objeto tipo Json y introduce los atributos correspondientes de direccion
+	 * @param prov: Recoge el objeto tipo Json y introduce los atributos correspondientes de Provincia
+	 */
 	@PostMapping
-	public void add(@ModelAttribute Persona pers, @ModelAttribute Telefono tel, @ModelAttribute Direccion dir) {
-		System.out.println("------------------------------------- he entrado al metodo pero no he guardado");
-		System.out.println(tel.toString());
-		System.out.println(pers.toString());
+	public void add(@ModelAttribute Persona pers, @ModelAttribute Telefono tel, @ModelAttribute Direccion dir, @ModelAttribute Provincia prov) {
 		
 		//INYECTAMOS DENTRO DEL TELEFONO AÑADIDO LA PERSONA A LA QUE VA A SER AÑADIDA
 		//CREAMOS UNA LISTA, A LA CUAL AÑADIMOS EL TELEFONO Y ESA LISTA LA INYECTAMOS EN PERSONA
@@ -40,12 +55,21 @@ public class AgendaController {
 		listaTelefonica.add(tel);
 		pers.setTelefonos(listaTelefonica);
 		
+		//INYECTAMOS DENTRO DE LA DIRECCION AÑADIDA LA PERSONA A LA QUE VA A SER AÑADIDA
+		//CREAMOS UNA LISTA, A LA CUAL AÑADIMOS LA DIRECCION Y ESA LISTA LA INYECTAMOS EN PERSONA
 		dir.setPersona(pers);
 		List<Direccion> listaDirecciones = new ArrayList<>();
-		listaDirecciones.add(dir);
+		listaDirecciones.add(dir);	
+		
+		//A LA PROVINCIA CORRESPONDIENTE, DEL CUAL COGEMOS EL ID PROVINCIA, LE ASIGNAMOS UNA DIRECCION
+		//Y ESA PROVINCIA A SU VEZ, LA RELACIONAMOS CON UNA DIRECCION
+		prov.setDireccions(listaDirecciones);
+		dir.setProvincia(prov);
+		
+		//FINALEMENTE INYECTAMOS LA LISTA DE DIRECCIONES A PERSONAS
 		pers.setDireccions(listaDirecciones);
 		
+		//AÑADIMOS DENTRO DE LA BASE DE DATOS LA PERSONA JUNTO CON LA DIRECCION Y SU PROVINCIA ASOCIADA Y EL TELEFONO
 		servicios.add(pers);
-		System.out.println("------------------------------------- he guardado, soy dios");
 	}
 }
--- Server-RestAgenda/src/main/java/com/spring/agenda/model/Provincia.java
+++ Server-RestAgenda/src/main/java/com/spring/agenda/model/Provincia.java
@@ -73,5 +73,11 @@ public class Provincia implements Serializable {
 
 		return direccion;
 	}
+	@Override
+	public String toString() {
+		return "Provincia [idprovincia=" + idprovincia + ", provincia=" + provincia + ", direccions=" + direccions
+				+ "]";
+	}
 
+	
 }
