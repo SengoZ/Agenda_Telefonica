3d1c36fe660d2ef2e316dd62f1786aac04be6e02
admin <ms.garcia.2018@alumnos.urjc.es>
2019-03-28 09:15:56 +0100

    Ya no hace falta el plugging de Chrome

--- Server-RestAgenda/src/main/java/com/spring/agenda/control/AgendaController.java
+++ Server-RestAgenda/src/main/java/com/spring/agenda/control/AgendaController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -23,6 +24,7 @@ import com.spring.agenda.services.AgendaService;
  * @author Grupo 3: Amador Cáceres, Cesar Marcos, Andrés Gomez y Sheila García
  *
  */
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping({"/person"})
 public class AgendaController {
